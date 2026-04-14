a34573b2c87426b1b8d90abf7893e81f9fc6414b
smRid <omorsk911@gmail.com>
2026-04-14 13:22:14 +0600

    footer update

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
@@ -6,6 +7,7 @@ const page = () => {
     <div>
       <Navbar />
       Timeline Page
+      <Footer />
     </div>
   );
 };
